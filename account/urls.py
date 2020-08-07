0e136729d8bf53f97f2366a7774933f0d54f5bf4
Min-Ho <zocracker@icloud.com>
2020-08-07 20:46:10 +0900

    Add: kakao social login view

--- account/urls.py
+++ account/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 
 from .views import (
     SignUpView,
-    SignInView
+    SignInView,
+    KakaoSignInView,
     ProfileView
 )
 
 urlpatterns = [
     path('/sign-up', SignUpView.as_view()),
-    path('/sign-in', SignInView.as_view())
+    path('/sign-in', SignInView.as_view()),
+    path('/kakao', KakaoSignInView.as_view()),
     path('/@<user_name>', ProfileView.as_view())
 ]
